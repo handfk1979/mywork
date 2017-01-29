da7e4d8af4969df98d5836b5a3d40d7c4541a9d1
ygapp <yuliang@yingu.com>
2017-01-29 10:21:25 +0800

    add new file

---
+++ main/java/com/mywork/tests/mywork.java
@@ -0,0 +1,15 @@
+package com.mywork.tests;
+
+/**
+ * Created by User on 2017/1/29.
+ */
+
+public class mywork {
+    public mywork(String name){
+        System.out.println("hello "+ name);
+    }
+
+    public static void main(String args[]){
+        new mywork("steven");
+    }
+}
